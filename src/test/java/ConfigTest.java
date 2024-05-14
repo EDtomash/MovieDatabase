5973bfa5538ab7a6deedfc515ed83eb361f09427
Dixon <dixonkabass@gmail.com>
2024-05-14 00:38:04 -0400

    Implementing headless-mod

--- src/test/java/ConfigTest.java
+++ src/test/java/ConfigTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
@@ -10,11 +11,13 @@ public class ConfigTest {
     String userName = "wertertdfgdfg";
     String userPassword = "12301230";
 
-    @BeforeMethod
+    @BeforeClass
     public void setup() {
-        Configuration.browser = "chrome";
+        Configuration.browser = System.getProperty("selenide.browser", "chrome");
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = false;
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("selenide.headless", "false"));
+        System.out.println("Browser: " + Configuration.browser);
+        System.out.println("Headless: " + Configuration.headless);
         open("https://www.themoviedb.org");
     }
 
